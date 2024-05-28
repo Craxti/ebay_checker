15543ef75c359b29c2e3eccfe4eaa3ff98d8ae36
Aleksandr Fetisov <afetisov@prooftechit.com>
2024-05-28 14:29:47 +0300

    fix main.py

--- main.py
+++ main.py
@@ -24,42 +24,47 @@ logger.add(
 """
 
 
-async def main(proxy: bool) -> None:
+async def main(proxy, good_account_file: str) -> None:
     accounts: list = await parser.account.getAccounts(
         filename=parser.config.ACCOUNTS_FILE
     )
-    for number in range(len(accounts)):
-        account: list = accounts[number]
-        login_pass = account.split(":")
+    for account in accounts:
+        login_pass = account.strip().split(":")
         test = await parser.ebay_checker.login(
             account_data=login_pass,
             url=parser.config.EBAY_LOGIN_URL,
             bad_account_file=parser.config.BAD_ACCOUNTS_OUTPUT_FILE,
+            good_account_file=good_account_file  # Передаем файл для записи успешных аккаунтов
         )
     return None
 
 
 while __name__ == "__main__":
     logger.info("Parser started")
-    thread: int = input("How many processes you need to run: ")
-    use_proxy: str = input("Use proxy?(Y/n) ")
+    thread: int = int(input("How many processes you need to run: "))
+    use_proxy = input("Use proxy?(Y/n) ")
+    if use_proxy == "Y":
+        use_proxy = True
+    else:
+        use_proxy = False
+    good_account_filename = input("Enter the filename for storing good accounts: ")
 
     if not isinstance(thread, int):
         print(f"Error input value ({thread})")
 
         if use_proxy == "Y" or "y":
-            use_proxy: bool = True
+            use_proxy = True
             break
 
         elif use_proxy == "N" or "n":
-            use_proxy: bool = False
+            use_proxy = False
             break
 
         else:
             print("Type Y or N")
 
-        for i in thread:
-            multiprocessing.Process(target=asyncio.run(main(proxy=use_proxy)))
+        for _ in range(thread):
+            multiprocessing.Process(target=asyncio.run(main(proxy=use_proxy, good_account_file=good_account_filename)))
             # asyncio.run(main())
 
     logger.warning("Parser shutdown")
